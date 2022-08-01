3e1631afc71ff05d059d5cb6d2e5e01e9893c26a
Paul Taylor <paul@whammydiver.com>
2022-08-01 19:36:19 -0400

    add seed file

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -1,29 +1,29 @@
---INSERT INTO users (name, email, password) VALUES ('Paul Taylor', 'taylor@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
---INSERT INTO users (name, email, password) VALUES ('Kendall Smigh', 'smith@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
---INSERT INTO users (name, email, password) VALUES ('Logan Nova', 'nova@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
---INSERT INTO users (name, email, password) VALUES ('Boaty McBoatface', 'boaty@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
---INSERT INTO users (name, email, password) VALUES ('Rick Astley', 'yougotrickrolled@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+INSERT INTO users (name, email, password) VALUES ('Paul Taylor', 'taylor@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+INSERT INTO users (name, email, password) VALUES ('Kendall Smigh', 'smith@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+INSERT INTO users (name, email, password) VALUES ('Logan Nova', 'nova@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+INSERT INTO users (name, email, password) VALUES ('Boaty McBoatface', 'boaty@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+INSERT INTO users (name, email, password) VALUES ('Rick Astley', 'yougotrickrolled@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
 
---INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, 
---  cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, 
---  city, province, postal_code, active) 
---  VALUES (
---    1, 
---    'Abbotsford Retreat', 
---    'long ass description', 
---    'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg?auto=compress&cs=tinysrgb&h=350',
---    'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg',
---    300,
---    1,
---    2,
---    2,
---    'Canada',
---    '123 King Road',
---    'Abbotsford',
---    'British Columbia',
---    'A1A 1A1',
---    TRUE
---  );
+INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, 
+  cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, 
+  city, province, postal_code, active) 
+  VALUES (
+    1, 
+    'Abbotsford Retreat', 
+    'long ass description', 
+    'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg?auto=compress&cs=tinysrgb&h=350',
+    'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg',
+    300,
+    1,
+    2,
+    2,
+    'Canada',
+    '123 King Road',
+    'Abbotsford',
+    'British Columbia',
+    'A1A 1A1',
+    TRUE
+ );
 
 INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, 
   cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, 
