aff2d5ddd3f01e6c625d188222dcb76d607261ef
Paul Taylor <paul@whammydiver.com>
2022-08-04 18:32:41 -0400

    includes project web_app

--- 1_queries/allMyReservations.sql
+++ 1_queries/allMyReservations.sql
@@ -2,7 +2,6 @@ SELECT reservations.id, properties.title, properties.cost_per_night, reservation
 FROM reservations
 JOIN properties ON reservations.property_id = properties.id
 JOIN property_reviews ON properties.id = property_reviews.property_id
-JOIN users ON users.id = reservations.guest_id
 WHERE reservations.guest_id = 1
 GROUP BY reservations.id, properties.title, properties.cost_per_night
 ORDER BY reservations.start_date
