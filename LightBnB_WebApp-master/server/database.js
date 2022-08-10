1a893dae1ac457870098e0e8f3c08cfdc9621b38
Paul Taylor <paul@whammydiver.com>
2022-08-09 21:42:36 -0400

    refactor getUserWithEmail, getUserWithId, addUser

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -14,30 +14,36 @@ const pool = new Pool({
 
 /**
  * Get a single user from the database given their email.
- * @param {String} email The email of the user.
- * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function(email) {
-  let user;
-  for (const userId in users) {
-    user = users[userId];
-    if (user.email.toLowerCase() === email.toLowerCase()) {
-      break;
-    } else {
-      user = null;
-    }
-  }
-  return Promise.resolve(user);
+  return pool
+    .query(
+      `SELECT * FROM users WHERE email = $1`,
+      [email])
+    .then((result) => {
+      return result.rows[0];
+    })
+    .catch((err) => {
+      return(err.message);
+    });
 }
 exports.getUserWithEmail = getUserWithEmail;
 
 /**
  * Get a single user from the database given their id.
- * @param {string} id The id of the user.
- * @return {Promise<{}>} A promise to the user.
+
  */
 const getUserWithId = function(id) {
-  return Promise.resolve(users[id]);
+  return pool
+    .query(
+      `SELECT * FROM users WHERE id = $1`,
+      [id])
+    .then((result) => {
+      return result.rows[0];
+    })
+    .catch((err) => {
+      return(err.message);
+    });
 }
 exports.getUserWithId = getUserWithId;
 
@@ -48,10 +54,19 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser =  function(user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  console.log(user.name, user.email, user.password);
+  return pool
+    .query(
+      `INSERT INTO users (name, email, password) VALUES ($1, $2, $3)
+      RETURNING *;`,
+      [user.name, user.email, user.password])
+    .then((result) => {
+      return result.rows[0];
+    })
+    .catch((err) => {
+      console.log(err.message);
+      return(err.message);
+    });
 }
 exports.addUser = addUser;
 
@@ -81,11 +96,9 @@ exports.getAllReservations = getAllReservations;
       `SELECT * FROM properties LIMIT $1`,
       [limit])
     .then((result) => {
-      console.log(result.rows);
       return result.rows;
     })
     .catch((err) => {
-      console.log(err.message);
       return(err.message);
     });
 };
